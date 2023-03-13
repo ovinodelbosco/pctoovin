237dac4fa101a9235e1df7a96c153044b224e93c
ovinodelbosco <127745108+ovinodelbosco@users.noreply.github.com>
2023-03-13 15:47:54 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int numero = 69;
+double numerolungo = 6969696969;
+float numeroconlavirgola = 6.969;
+char carattere = 'c';
+
+printf("il numero e: %d\n", numero);
+printf("numero lungo e: %f, numero con la virgola e: %f\n", numerolungo, numeroconlavirgola);
+printf("carattere e: %c\n", carattere);
+}
