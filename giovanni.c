f20c1f7d1be365677687631af81c1fdd61556a3c
ovinodelbosco <127745108+ovinodelbosco@users.noreply.github.com>
2023-03-14 10:10:52 +0100

    Add files via upload

---
+++ giovanni.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int x;
+char y;
+printf ("inserisci i tuoi anni");
+scanf("%d", &x);
+printf("inserisci il tuo sesso");
+scanf(" %c", &y);
+printf("io ho %d anni\n e sono %c", x, y);
+
+}
